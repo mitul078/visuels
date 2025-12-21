2698e9ec7dcc8bcdde0236aa0d73acd9d833fb85
mitul078 <mituljodhani788@gmail.com>
2025-12-21 14:57:56 +0530

    feat(cart): update cart items with new price and recalculate totals on price change event

--- server/cart/src/broker/listeners.js
+++ server/cart/src/broker/listeners.js
@@ -1,9 +1,60 @@
 const { subscribeToQueue } = require("./broker")
 const Cart = require("../modules/cart.model")
+const mongoose = require("mongoose")
 
 
 module.exports = () => {
     subscribeToQueue("PRODUCT_SERVICE:PRICE_CHANGED", async (data) => {
+        const { productId, price } = data
+        const pid = new mongoose.Types.ObjectId(productId)
         console.log(data)
+        await Cart.updateMany(
+            { "items.productId": pid },
+            [
+                {
+                    $set: {
+                        items: {
+                            $map: {
+                                input: "$items",
+                                as: "item",
+                                in: {
+                                    $cond: [
+                                        {
+                                            $eq: ["$$item.productId", pid]
+                                        },
+                                        {
+                                            $mergeObjects: [
+                                                "$$item",
+                                                {
+                                                    price: price,
+                                                    subtotal: {
+                                                        $multiply: ["$$item.quantity", price]
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                        "$$item"
+                                    ]
+                                }
+                            }
+
+                        }
+                    }
+                },
+                {
+                    $set: {
+                        totalItems: {
+                            $sum: "$items.quantity"
+                        },
+                        totalPrice: {
+                            $sum: "$items.subtotal"
+                        }
+                    }
+                }
+            ],
+            {
+                updatePipeline: true
+            }
+        )
     })
 }
